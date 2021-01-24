ea4855707f2ae762d42def1392b08e2d808cc180
bouguerra-abs <bouguerraabdsatar2050@gmail.com>
2021-01-24 02:37:54 +0100

    modify the serviceProvider file to fix the errror bootstrap and image on heroku app

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,7 +29,7 @@ class AppServiceProvider extends ServiceProvider
         Schema::defaultStringLength(191);
 
         if(env('APP_ENV') !== 'local') {
-            URL::forceScheme( scheme, 'https');
+            URL::forceScheme('https');
         }
     }
 }
